f3f42f8834a3e56e7383549ae953a89700dc3bee
Javier <tuya@uniovi.es>
2023-08-17 17:27:09 +0200

    Add full client api transformed from java to net

--- tdrules-client/src/main/java/giis/tdrules/client/TdRulesCacheManager.java
+++ tdrules-client/src/main/java/giis/tdrules/client/TdRulesCacheManager.java
@@ -28,6 +28,8 @@ public class TdRulesCacheManager {
 
 	public void setCache(String location) {
 		this.useCache = !"".equals(coalesce(location));
+		if (!this.useCache)
+			return;
 		// Gets version (only first time) to put as part of the cache folder
 		if ("".equals(version)) {
 			version = new TdRulesApi(endpoint).getVersion().getServiceVersion(); // NOSONAR only for non concurrent
