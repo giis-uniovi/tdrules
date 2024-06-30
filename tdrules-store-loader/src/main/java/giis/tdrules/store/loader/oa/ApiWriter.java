13b82929d7e43a595e3731cdf36a5a69afc527e4
Javier <10879637+javiertuya@users.noreply.github.com>
2024-06-30 08:14:20 +0200

    Migrate httpclient to v5

--- tdrules-store-loader/src/main/java/giis/tdrules/store/loader/oa/ApiWriter.java
+++ tdrules-store-loader/src/main/java/giis/tdrules/store/loader/oa/ApiWriter.java
@@ -1,26 +1,24 @@
 package giis.tdrules.store.loader.oa;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import org.apache.hc.client5.http.classic.methods.HttpDelete;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.classic.methods.HttpPut;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 
 import giis.tdrules.store.loader.LoaderException;
 
 /**
- * Utilidades para envio (y recepcion) de datos a un api
+ * Utility to send/receive json data though an API
  */
 public class ApiWriter {
 	// Headers to sent (in addition to Content-type: application/json)
@@ -30,7 +28,7 @@ public class ApiWriter {
 		headers.add(new String[] { key, value });
 		return this;
 	}
-	
+
 	public List<String[]> getHeaders() {
 		return headers;
 	}
@@ -40,45 +38,40 @@ public class ApiWriter {
 		String encodedCredential = "Basic " + Base64.getEncoder().encodeToString(credentialToEncode.getBytes());
 		return addHeader("Authorization", encodedCredential);
 	}
-	
-    public ApiResponse post(String url, String requestBody, boolean usePut) {
-    	HttpEntityEnclosingRequestBase post = usePut ? new HttpPut(url) : new HttpPost(url);
-		try { //el post requiere confeccionar el body como StringEntity
-			StringEntity stringEntity = new StringEntity(requestBody);
-			post.getRequestLine();
-			post.setEntity(stringEntity);
-		} catch (UnsupportedEncodingException e) {
-            throw new LoaderException(e);
- 		}
-        return apiExecute(post);
-    }
-    
-    public ApiResponse get(String url) {
-        HttpGet get = new HttpGet(url);
-    	return apiExecute(get);
-    }
 
-    public ApiResponse delete(String url) {
-    	HttpDelete delete=new HttpDelete(url);
-    	return apiExecute(delete);
-    }
-    
-    private ApiResponse apiExecute(HttpUriRequest request) {
-        HttpClient client = HttpClientBuilder.create().build();
-        request.setHeader("Content-type", "application/json");
-        for (String[] header : this.headers) // adds other headers if configured
-        	request.setHeader(header[0], header[1]);
-        ApiResponse apiResponse=null;
+	public ApiResponse post(String url, String requestBody, boolean usePut) {
+		HttpUriRequestBase post = usePut ? new HttpPut(url) : new HttpPost(url);
+		StringEntity stringEntity = new StringEntity(requestBody);
+		post.getRequestUri();
+		post.setEntity(stringEntity);
+		return apiExecute(post);
+	}
 
-        try {
-            HttpResponse response=client.execute(request);
-            apiResponse = new ApiResponse(response.getStatusLine().getStatusCode(),
-            		response.getStatusLine().getReasonPhrase(),
-            		EntityUtils.toString(response.getEntity()));
-        } catch (Exception e) {
-            throw new LoaderException(e);
-        }
-        return apiResponse;
-    }
+	public ApiResponse get(String url) {
+		HttpGet get = new HttpGet(url);
+		return apiExecute(get);
+	}
+
+	public ApiResponse delete(String url) {
+		HttpDelete delete = new HttpDelete(url);
+		return apiExecute(delete);
+	}
+
+	private ApiResponse apiExecute(HttpUriRequest request) {
+		request.setHeader("Content-type", "application/json");
+		for (String[] header : this.headers) // adds other headers if configured
+			request.setHeader(header[0], header[1]);
+
+		ApiResponse result;
+		try (CloseableHttpClient client = HttpClients.createDefault()) {
+			result = client.execute(request, response -> 
+				new ApiResponse(response.getCode(), response.getReasonPhrase(),
+					EntityUtils.toString(response.getEntity())
+				));
+		} catch (Exception e) {
+			throw new LoaderException(e);
+		}
+		return result;
+	}
 
 }
