ffea226418be1da767c2b7d132524216a6603e38
Javier <10879637+javiertuya@users.noreply.github.com>
2025-06-08 12:10:16 +0200

    Refactor load adapter tests

--- tdrules-store-loader/src/test/java/test4giis/tdrules/store/loader/oa/TestOaLiveGeneration.java
+++ tdrules-store-loader/src/test/java/test4giis/tdrules/store/loader/oa/TestOaLiveGeneration.java
@@ -1,8 +1,6 @@
 package test4giis.tdrules.store.loader.oa;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 import static org.mockserver.matchers.Times.exactly;
 import static org.mockserver.model.HttpRequest.request;
@@ -28,15 +26,20 @@ import giis.tdrules.store.loader.oa.OaBasicAuthStore;
 import giis.tdrules.store.loader.oa.OaLiveAdapter;
 import giis.tdrules.store.loader.oa.OaLiveUidGen;
 import giis.tdrules.store.loader.oa.OaPathResolver;
-import giis.tdrules.store.loader.shared.LoaderException;
 import giis.tdrules.openapi.model.TdEntity;
 
 /**
  * Data generation and loading with a live adapter.
- * As tests for different models with a local adapter are in a separate class,
- * this focus on different situations regarding other features like
- * authentication, customization of the path resolver and errors.
- * DO NOT focus on the uid generation at the backend (in a subclass)
+ * Tests for different models with a local adapter are in a separate class,
+ * this focus on different situations regarding 
+ * - Sequential uid generator (frontend generated uid), all tests
+ * - distinctive features of live generation like authentication, 
+ *   path parameters, customization of the path resolver
+ * - already tested features where live generation could impact, like
+ *   the handling of symbolic keys
+ * This does not focus on (tested in separated classes):
+ * - Backend generated uids.
+ * - Error handling
  */
 public class TestOaLiveGeneration extends Base {
 	// A single MockServer for this class, each test initializes its expectation before post
@@ -69,12 +72,9 @@ public class TestOaLiveGeneration extends Base {
 	}
 
 	// Default methods to get the data generator (some tests require other customized versions)
-	protected DataLoader getLiveGenerator() {
-		return getLiveGenerator(getModel());
-	}
 	protected DataLoader getLiveGenerator(TdSchema schema) {
 		IDataAdapter dataAdapter=new OaLiveAdapter(getServerUrl());
-		return new DataLoader(schema, dataAdapter).setUidGen(new OaLiveUidGen());
+		return new DataLoader(schema, dataAdapter);
 	}
 	protected String getServerUrl() {
 		return "http://127.0.0.1:" + mockServer.getPort() + "/oatest";
@@ -82,12 +82,12 @@ public class TestOaLiveGeneration extends Base {
 	
 	@Test
 	public void testGenerateLiveValues() {
-		DataLoader dtg=getLiveGenerator();
+		DataLoader dtg=getLiveGenerator(getModel());
 		// expected input and output in the mock, if input (generated object) is not the expected, returns 404
-		createExpectationPost("/master", "{'pk1':1,'I1':2}", "{'pk1':1,'I1':2}");
+		mockPost("/master", "{'pk1':1,'I1':2}", "{'pk1':1,'I1':2}");
 		String json1 = dtg.load("master", "", "");
 		assertJson("{'pk1':1,'I1':2}", json1);
-		createExpectationPost("/master", "{'pk1':101,'I1':102}", "{'pk1':101,'I1':102}");
+		mockPost("/master", "{'pk1':101,'I1':102}", "{'pk1':101,'I1':102}");
 		String json2 = dtg.load("master", "", "");
 		assertJson("{'pk1':101,'I1':102}", json2);
 		
@@ -99,9 +99,9 @@ public class TestOaLiveGeneration extends Base {
 		// Reset, reset of the data sequence
 		// Note that the uid is not generated in the backend because there is no symbolic values
 		dtg.reset();
-		createExpectationPost("/master", "{'pk1':1,'I1':2}", "{'pk1':1,'I1':2}");
+		mockPost("/master", "{'pk1':1,'I1':2}", "{'pk1':1,'I1':2}");
 		dtg.load("master", "", "");
-		createExpectationPost("/master", "{'pk1':101,'I1':102}", "{'pk1':101,'I1':102}");
+		mockPost("/master", "{'pk1':101,'I1':102}", "{'pk1':101,'I1':102}");
 		dtg.load("master", "", "");
 		assertEquals(expected, dtg.getDataAdapter().getAllAsString());
 	}
@@ -113,7 +113,7 @@ public class TestOaLiveGeneration extends Base {
 		schema.getEntity("master").getAttribute("I1").setReadonly("true");
 		DataLoader dtg=getLiveGenerator(schema);
 		// mock only accepts an object without the readonly I1, but returns I1
-		createExpectationPost("/master", "{'pk1':1,'I2':3}", "{'pk1':1,'I1':2,'I2':3}");
+		mockPost("/master", "{'pk1':1,'I2':3}", "{'pk1':1,'I1':2,'I2':3}");
 		String json1 = dtg.load("master", "", "");
 		assertJson("{'pk1':1,'I2':3}", json1);
 		// Note difference between getLast and getLastGenerated
@@ -131,11 +131,11 @@ public class TestOaLiveGeneration extends Base {
 		DataLoader dtg=getLiveGenerator(schema);
 		
 		// path parameter values are in the url, not in the request body
-		createExpectationPost("/master/2/3", "{'pk1':1}", "{'pk1':1,'I1':2,'s2':'3'}");
+		mockPost("/master/2/3", "{'pk1':1}", "{'pk1':1,'I1':2,'s2':'3'}");
 		String json1 = dtg.load("master", "", "");
 		assertJson("{'pk1':1,'I1':2,'s2':'3'}", json1);
 		// check parameter values are url encoded in the path
-		createExpectationPost("/master/102/ab%3Fcd", "{'pk1':101}", "{'pk1':101,'I1':102,'s2':'ab?cd'}");
+		mockPost("/master/102/ab%3Fcd", "{'pk1':101}", "{'pk1':101,'I1':102,'s2':'ab?cd'}");
 		String json2 = dtg.load("master", "s2=ab?cd");
 		assertJson("{'pk1':101,'I1':102,'s2':'ab?cd'}", json2);
 		
@@ -170,17 +170,17 @@ public class TestOaLiveGeneration extends Base {
 		// The custom path resolver changes the path of master, 
 		// and prevents sending data thorought the api for detail (althoug generation is still done)
 		DataLoader dtg=getLiveGeneratorWithCustomPathResolver(getModel());
-		createExpectationPost("/surrogate/master", "{'pk1':1,'I1':2}", "{'pk1':1,'I1':2}");
+		mockPost("/surrogate/master", "{'pk1':1,'I1':2}", "{'pk1':1,'I1':2}");
 		String json1 = dtg.load("master", "", "");
 		assertJson("{'pk1':1,'I1':2}", json1);
 		
 		// This is the post that is not sent, but generation is done 
-		createExpectationPost("/detail", "{}", "{}");
+		mockPost("/detail", "{}", "{}");
 		String json2 = dtg.load("detail", "fk1=1");
 		assertJson("{'pk1':101,'fk1':1,'i1':103}", json2);
 
 		// Checks that generation continues at another entity
-		createExpectationPost("/surrogate/master", "{'pk1':201,'I1':202}", "{'pk1':201,'I1':202}");
+		mockPost("/surrogate/master", "{'pk1':201,'I1':202}", "{'pk1':201,'I1':202}");
 		String json3 = dtg.load("master", "", "");
 		assertJson("{'pk1':201,'I1':202}", json3);
 
@@ -197,7 +197,7 @@ public class TestOaLiveGeneration extends Base {
 		schema.getEntity("Master").name("Master2");
 
 		DataLoader dtg = getLiveGeneratorWithCustomPathResolver(schema);
-		createSuccessExpectation("/master2", "{'pk1':1,'I1':2}", "{'pk1':1,'I1':2}", "PUT"); // expects put
+		mockOperation("/master2", "{'pk1':1,'I1':2}", "{'pk1':1,'I1':2}", "PUT"); // expects put
 		String json1 = dtg.load("master2", "", "");
 		assertJson("{'pk1':1,'I1':2}", json1);
 		assertEquals("\"master2\":{\"pk1\":1,\"I1\":2}", dtg.getDataAdapter().getAllAsString());
@@ -222,14 +222,14 @@ public class TestOaLiveGeneration extends Base {
 		((OaLiveAdapter) dtg.getDataAdapter()).setAuthStore(authenticator);
 	
 		// Post that remembers credentials of usr1
-		createExpectationPost("/master", "{'pk1':1,'I1':2,'username':'usr1','password':'abc'}",
+		mockPost("/master", "{'pk1':1,'I1':2,'username':'usr1','password':'abc'}",
 				"{'pk1':1,'I1':2,'username':'usr1','password':'abc'}");
 		String json1 = dtg.load("Master", "username=usr1, password=abc");
 		assertJson("{'pk1':1,'I1':2,'username':'usr1','password':'abc'}", json1);
 	
 		// Post with a user to get credentials from and auth header in the request
 		String header = "Basic " + Base64.getEncoder().encodeToString("usr1:abc".getBytes());
-		createExpectationPostWithAuth("/detail", "{'pk1':101,'fk1':1,'i1':103,'user':'usr1'}",
+		mockPostWithAuth("/detail", "{'pk1':101,'fk1':1,'i1':103,'user':'usr1'}",
 				"{'pk1':101,'fk1':1,'i1':103,'user':'usr1'}", header);
 		String json2 = dtg.load("Detail", "fk1=1,user=usr1");
 		assertJson("{'pk1':101,'fk1':1,'i1':103,'user':'usr1'}", json2);
@@ -237,116 +237,71 @@ public class TestOaLiveGeneration extends Base {
 	
 	@Test
 	public void testGenerateLiveSymbolicKeys() {
-		DataLoader dtg=getLiveGenerator();
+		DataLoader dtg=getLiveGenerator(getModel());
 		
-		// Scenario with two master entities generate uids, two details referecing each.
+		// Scenario with a master and an entity referencing it, two objects for each
 		// Each step configures the mock server to return the same object plus the generted uid
 		// Check the json generated and the backend generated uid
-		createExpectationPost("/master", "{'I1':10}", "{'pk1':991,'I1':10}");
-		String json1 = dtg.load("master", "Pk1,i1", "@km1,10");
-		//el valor devuelto por load es el enviado, la clave del objeto creado esta en las symbolic keys
-		assertJson("{'I1':10}", json1);
+		
+		//Sequential uid generator sets 1 for the first object created
+		mockPost("/master", "{'pk1':1,'I1':10}", "{'pk1':1,'I1':10}");
+		String json1 = dtg.load("master", "Pk1=@km1, i1=10");
+		assertJson("{'pk1':1,'I1':10}", json1);
 		assertEquals(1, dtg.getSymbolicKeyValues().size());
-		assertJson("991", dtg.getSymbolicKeyValues().get("master.pk1.@km1"));
+		assertJson("1", dtg.getSymbolicKeyValues().get("master.pk1.@km1"));
 
-		createExpectationPost("/master", "{'I1':20}", "{'pk1':992,'I1':20}");
-		String json2 = dtg.load("master", "pk1,i1", "@km2,20");
-		assertJson("{'I1':20}", json2);
+		//next in sequence (2)
+		mockPost("/master", "{'pk1':2,'I1':20}", "{'pk1':992,'I1':20}");
+		String json2 = dtg.load("master", "pk1=@km2, i1=20");
+		assertJson("{'pk1':2,'I1':20}", json2);
 		assertEquals(2, dtg.getSymbolicKeyValues().size());
-		assertJson("992", dtg.getSymbolicKeyValues().get("master.pk1.@km2"));
+		assertJson("2", dtg.getSymbolicKeyValues().get("master.pk1.@km2"));
 		
-		createExpectationPost("/detail", "{'fk1':992,'i1':100}", "{'pk1':881,'fk1':992,'i1':100}");
-		String json3 = dtg.load("detail", "pk1,fk1,i1", "@kd1,@km2,100");
-		assertJson("{'fk1':992,'i1':100}", json3);
+		//a different entity, starts its own sequence
+		mockPost("/detail", "{'pk1':1,'fk1':2,'i1':100}", "{'pk1':1,'fk1':2,'i1':100}");
+		String json3 = dtg.load("detail", "pk1=@kd1, fk1=@km2, i1=100");
+		assertJson("{'pk1':1,'fk1':2,'i1':100}", json3);
 		assertEquals(3, dtg.getSymbolicKeyValues().size());
-		assertJson("881", dtg.getSymbolicKeyValues().get("detail.pk1.@kd1"));
+		assertJson("1", dtg.getSymbolicKeyValues().get("detail.pk1.@kd1"));
 		
-		createExpectationPost("/detail", "{'fk1':991,'i1':200}", "{'pk1':882,'fk1':991,'i1':200}");
-		String json4 = dtg.load("detail", "pk1,fk1,i1", "@kd2,@km1,200");
-		assertJson("{'fk1':991,'i1':200}", json4);
+		mockPost("/detail", "{'pk1':2,'fk1':1,'i1':200}", "{'pk1':2,'fk1':991,'i1':200}");
+		String json4 = dtg.load("detail", "pk1=@kd2 ,fk1=@km1 ,i1=200");
+		assertJson("{'pk1':2,'fk1':1,'i1':200}", json4);
 		assertEquals(4, dtg.getSymbolicKeyValues().size());
-		assertJson("882", dtg.getSymbolicKeyValues().get("detail.pk1.@kd2"));
+		assertJson("2", dtg.getSymbolicKeyValues().get("detail.pk1.@kd2"));
 		
 		// Comprueba el resultado global como string
-		String expected = "\"master\":{\"I1\":10}\n"
-				+ "\"master\":{\"I1\":20}\n"
-				+ "\"detail\":{\"fk1\":992,\"i1\":100}\n"
-				+ "\"detail\":{\"fk1\":991,\"i1\":200}";
+		String expected = "\"master\":{\"pk1\":1,\"I1\":10}\n"
+				+ "\"master\":{\"pk1\":2,\"I1\":20}\n"
+				+ "\"detail\":{\"pk1\":1,\"fk1\":2,\"i1\":100}\n"
+				+ "\"detail\":{\"pk1\":2,\"fk1\":1,\"i1\":200}";
 		assertEquals(expected, dtg.getDataAdapter().getAllAsString());
 		
-		// Reset, only cheking two first steps
+		// Reset, only chek two first steps, sequence starts again at 1
 		dtg.reset();
-		createExpectationPost("/master", "{'I1':10}", "{'pk1':991,'I1':10}");
-		dtg.load("master", "Pk1,i1", "@km1,10");
+		mockPost("/master", "{'pk1':1,'I1':10}", "{'pk1':1,'I1':10}");
+		dtg.load("master", "Pk1=@km1, i1=10");
+		assertJson("{'pk1':1,'I1':10}", json1);
 		assertEquals(1, dtg.getSymbolicKeyValues().size());
-		assertJson("991", dtg.getSymbolicKeyValues().get("master.pk1.@km1"));
+		assertJson("1", dtg.getSymbolicKeyValues().get("master.pk1.@km1"));
 
-		createExpectationPost("/master", "{'I1':20}", "{'pk1':992,'I1':20}");
-		dtg.load("master", "pk1,i1", "@km2,20");
+		mockPost("/master", "{'pk1':2,'I1':20}", "{'pk1':992,'I1':20}");
+		dtg.load("master", "pk1=@km2, i1=20");
+		assertJson("{'pk1':2,'I1':20}", json2);
 		assertEquals(2, dtg.getSymbolicKeyValues().size());
-		assertJson("992", dtg.getSymbolicKeyValues().get("master.pk1.@km2"));
+		assertJson("2", dtg.getSymbolicKeyValues().get("master.pk1.@km2"));
 
-		expected = "\"master\":{\"I1\":10}\n"
-				+ "\"master\":{\"I1\":20}";
+		expected = "\"master\":{\"pk1\":1,\"I1\":10}\n"
+				+ "\"master\":{\"pk1\":2,\"I1\":20}";
 		assertEquals(expected, dtg.getDataAdapter().getAllAsString());
-}
-	
-	// Error situations: used some of the above scenarios, but injects an error
+	}
+		
+	// Utility methods to mock the api
 
-	@Test
-	public void testErrorClient() {
-		// Different path in client and serverin the client
-
-		LoaderException exception = assertThrows(LoaderException.class, () -> {
-			DataLoader dtg=getLiveGenerator();
-			createExpectationPost("/notexists", "{'I1':10}", "{'pk1':991,'I1':10}");
-			dtg.load("master", "pk1,i1", "@km1,10");
-	    });
-	    assertEquals("endWrite: Did not completed properly, response: 404 Not Found - body: "
-	    		+ "\n  Posting to: http://127.0.0.1:" + mockServer.getPort() + "/oatest/master"
-	    		+ "\n  With payload: {\"I1\":10}", json(exception.getMessage()));
+	private void mockPost(String endpointPath, String requestBody, String responseBody) {
+		mockOperation(endpointPath, requestBody, responseBody, "POST");
 	}
-	
-	@Test
-	public void testErrorServer() {
-		// expectation returns error 500
-		LoaderException exception = assertThrows(LoaderException.class, () -> {
-			DataLoader dtg = getLiveGenerator();
-			createErrorExpectationPost("/master", "{'I1':10}");
-			dtg.load("master", "pk1,i1", "@km1,10");
-		});
-	    assertEquals("endWrite: Did not completed properly, response: 500 Internal Server Error - body: Error message"
-	    		+ "\n  Posting to: http://127.0.0.1:" + mockServer.getPort() + "/oatest/master"
-	    		+ "\n  With payload: {\"I1\":10}", json(exception.getMessage()));
-	}
-	
-	@Test
-	public void testErrorCantSerializeResponse() {
-		LoaderException exception = assertThrows(LoaderException.class, () -> {
-			DataLoader dtg=getLiveGenerator();
-			createExpectationPost("/master", "{'I1':10}", "{ , 'pk1':991,'I1':10}");
-			dtg.load("master", "pk1,i1", "@km1,10");
-	    });
-	    assertTrue("Messaje does not start with expected JsonParseException, message was: "+exception.getMessage(), 
-	    		exception.getMessage().startsWith("com.fasterxml.jackson.core.JsonParseException: Unexpected character"));
-	}
-
-	@Test
-	public void testErrorCantFindGeneratedKey() {
-		LoaderException exception = assertThrows(LoaderException.class, () -> {
-			DataLoader dtg=getLiveGenerator();
-			createExpectationPost("/master", "{'I1':10}", "{'I1':10}");
-			dtg.load("master", "pk1,i1", "@km1,10");
-	    });
-	    assertJson("getLast: no generated value for entity: master - attribute: pk1 - Last response: {'I1':10}", exception.getMessage());
-	}
-	
-	// Utility methods to create expectations
-
-	private void createExpectationPost(String endpointPath, String requestBody, String responseBody) {
-		createSuccessExpectation(endpointPath, requestBody, responseBody, "POST");
-	}
-	private void createSuccessExpectation(String endpointPath, String requestBody, String responseBody, String method) {
+	private void mockOperation(String endpointPath, String requestBody, String responseBody, String method) {
 		mockServer.when(request()
 				.withMethod(method)
 				.withPath("/oatest" + endpointPath)
@@ -356,7 +311,7 @@ public class TestOaLiveGeneration extends Base {
 				.withBody(json(responseBody))
 		);
 	}
-	private void createExpectationPostWithAuth(String endpointPath, String requestBody, String responseBody, String authHeader) {
+	private void mockPostWithAuth(String endpointPath, String requestBody, String responseBody, String authHeader) {
 		mockServer.when(request()
 				.withMethod("POST")
 				.withPath("/oatest" + endpointPath)
@@ -365,17 +320,6 @@ public class TestOaLiveGeneration extends Base {
 				exactly(1)
 			).respond(response()
 				.withBody(json(responseBody))
-		);
-	}
-	private void createErrorExpectationPost(String endpointPath, String requestBody) {
-		mockServer.when(request()
-				.withMethod("POST")
-				.withPath("/oatest" + endpointPath)
-				.withBody(exact(json(requestBody))), 
-				exactly(1)
-			).respond(response()
-                .withStatusCode(500)
-				.withBody("Error message")
 		);
 	}
 
